43b1f275fade130e863ef7872f110825b25ababa
souravcog92 <sourav.sengupta@cognizant.com>
2026-06-05 20:30:04 +0530

    initial push

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
 
       {submitted && (
         <div className="alert alert-success mt-4" role="alert">
-          <strong>Submitted values:</strong>
+          <strong>Submitted values:</strong> hihihihihi
           <pre className="mb-0 mt-2">{JSON.stringify(submitted, null, 2)}</pre>
         </div>
       )}
